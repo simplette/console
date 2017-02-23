fd76bc2ea5c3b04adccdde3def9b26dbab0c2328
Lukas Povoda <lupo112@gmail.com>
2017-02-23 21:55:29 +0100

    BootstrapHelperTest

--- tests/DI/BootstrapHelperTest.php
+++ tests/DI/BootstrapHelperTest.php
@@ -1,15 +1,73 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: povod
- * Date: 23.02.2017
- * Time: 21:32
- */
 
 namespace Simplette\ConsoleTests\DI;
 
+use Mockery;
+use Mockery\Mock;
+use Nette\Configurator;
+use PHPUnit\Framework\TestCase;
+use Simplette\Console\DI\BootstrapHelper;
 
-class BootstrapHelperTest
+/**
+ * Class BootstrapHelperTest
+ *
+ * @package Simplette\ConsoleTests\DI
+ */
+class BootstrapHelperTest extends TestCase
 {
+
+	public function useCases()
+	{
+		return [
+			[['console', '--debug'], TRUE],
+			[['console'], FALSE],
+		];
+	}
+
+	/**
+	 * @dataProvider useCases
+	 *
+	 * @param array $argv
+	 * @param bool $useDebugMode
+	 */
+	public function testSetupMode(array $argv, $useDebugMode)
+	{
+		$_SERVER['argv'] = $argv;
+		/** @var Configurator|Mock $configurator */
+		$configurator = Mockery::mock(Configurator::class);
+		$configurator->shouldReceive('setDebugMode')
+			->with($useDebugMode)
+			->times($useDebugMode ? 1 : 0);
+
+		$called = 0;
+		BootstrapHelper::setupMode($configurator, function () use (&$called) {
+			$called++;
+		});
+		self::assertEquals($useDebugMode ? 0 : 1, $called);
+	}
+
+	/**
+	 * @dataProvider useCases
+	 * @doesNotPerformAssertions
+	 *
+	 * @param array $argv
+	 * @param bool $useDebugMode
+	 */
+	public function testSetupModeWithoutCallable(array $argv, $useDebugMode)
+	{
+		$_SERVER['argv'] = $argv;
+		/** @var Configurator|Mock $configurator */
+		$configurator = Mockery::mock(Configurator::class);
+		$configurator->shouldReceive('setDebugMode')
+			->with($useDebugMode)
+			->times($useDebugMode ? 1 : 0);
+
+		BootstrapHelper::setupMode($configurator);
+	}
+
+	protected function tearDown()
+	{
+		Mockery::close();
+	}
 
 }
